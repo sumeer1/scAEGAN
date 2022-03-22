0cd210a74dae243d4b66cdf652dfaf1019c11e11
Sumeer Khan <70262340+sumeer1@users.noreply.github.com>
2022-03-22 14:29:05 +0300

    Libraries

---
+++ Evaluation/R4_requirements.R
@@ -0,0 +1,11 @@
+# Package names
+packages <- c("ggplot2", "Seurat", "tidyverse", "cowplot", "pdfCluster", "clusteval", "scclusteval", "pheatmap", "cluster", "Rtsne", "umap", "e1071")
+
+# Install packages not yet installed
+installed_packages <- packages %in% rownames(installed.packages())
+if (any(installed_packages == FALSE)) {
+  install.packages(packages[!installed_packages])
+}
+
+# Packages loading
+invisible(lapply(packages, library, character.only = TRUE))
