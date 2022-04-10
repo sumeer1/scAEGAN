94b89ffaf700c98e3031dc8e1b1dbb9b69d5e84e
Sumeer Khan <70262340+sumeer1@users.noreply.github.com>
2022-04-10 15:51:22 +0300

    Update R_libraries.R

--- Evaluation/R_libraries.R
+++ Evaluation/R_libraries.R
@@ -1,11 +1,36 @@
 # Package names
-packages <- c("ggplot2", "Seurat", "tidyverse", "cowplot", "clusteval", "pdfCluster", "pheatmap", "cluster", "Rtsne", "umap", "e1071")
+# Package names
 
-# Install packages not yet installed
-installed_packages <- packages %in% rownames(installed.packages())
-if (any(installed_packages == FALSE)) {
-  install.packages(packages[!installed_packages])
+pkgLoad <- function( packages = "favourites" ) {
+
+    if( length( packages ) == 1L && packages == "favourites" ) {
+        packages <- c( "ggplot2", "Seurat", "tidyverse", "cowplot", "pdfCluster", "pheatmap", "cluster", "Rtsne", "umap", "e1071"
+        )
+    }
+
+    packagecheck <- match( packages, utils::installed.packages()[,1] )
+
+    packagestoinstall <- packages[ is.na( packagecheck ) ]
+
+    if( length( packagestoinstall ) > 0L ) {
+        utils::install.packages( packagestoinstall,
+                             repos = "http://cran.us.r-project.org"
+        )
+    } else {
+        print( "All requested packages from CRAN already installed" )
+    }
+    if( length( packagestoinstall) > 0L ) {
+	if (!requireNamespace("BiocManager", quietly = TRUE))
+    	install.packages("BiocManager")
+	BiocManager::install(packagestoinstall_2 )	
+    } else {
+        print( "All requested packages from BIOCONDUCTOR already installed" )
+    }
+
+    for( package in packages ) {
+        suppressPackageStartupMessages(
+            library( package, character.only = TRUE, quietly = TRUE )
+        )
+    }
 }
-
-# Packages loading
-invisible(lapply(packages, library, character.only = TRUE))
+pkgLoad("favourites")
